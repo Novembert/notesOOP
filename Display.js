805072331be3758a558793363ebe25ae68dd3054
Novembert <46356126+Novembert@users.noreply.github.com>
2019-03-21 17:47:02 +0100

    Add files via upload

--- Display.js
+++ Display.js
@@ -1,32 +1,55 @@
-const input = document.querySelector('input');
+const inputField = document.querySelector('input');
 const main = document.querySelector('main');
 
 class Display {
     constructor() {
-        let _table = [];
-        this.getTable = () => _table;
+        let _arrayOfNotes = [];
+        this.getArray = () => _arrayOfNotes;
 
-        this.removeNote = (e) => {
-            console.log(e.target.parentNode.dataset.key)
-            const index = e.target.parentNode.dataset.key;
-            _table.splice(index, 1);
+        this.findAndRemoveNote = (e) => {
+            const index = this.findIndexOfNote(e)
+            this.removeNoteFromArray(_arrayOfNotes, index)
             this.renderNotes();
         }
 
-        this.addNote = () => {
-            _table.push(new Note(input.value));
-            input.value = "";
-            _table[_table.length - 1].note.querySelector('button').addEventListener('click', this.removeNote);
+        this.addNoteAndClearInput = () => {
+            this.pushNoteToArray(_arrayOfNotes);
+            this.clearInput();
+            this.addDeleteEventToLastNoteInArray(_arrayOfNotes)
             this.renderNotes();
         }
         this.renderNotes = () => {
-            main.innerHTML = "";
-            _table.forEach((element, key) => {
-                element.note.dataset.key = key;
-                main.appendChild(element.note);
-            })
+            this.clearMain()
+            _arrayOfNotes.forEach(addKeyAndAppendNote(element, key))
         }
+    }
 
+    findIndexOfNote(e) {
+        return e.target.parentNode.dataset.key;
+    }
 
+    removeNoteFromArray(array, indexOfNote) {
+        array.splice(indexOfNote, 1)
+    }
+
+    pushNoteToArray(array) {
+        array.push(new Note(inputField.value));
+    }
+
+    clearInput() {
+        inputField.value = "";
+    }
+
+    addDeleteEventToLastNoteInArray(array) {
+        array[array.length - 1].note.querySelector('button').addEventListener('click', this.findAndRemoveNote);
+    }
+
+    clearMain() {
+        main.innerHTML = "";
+    }
+
+    addKeyAndAppendNote(element, key) {
+        element.note.dataset.key = key;
+        main.appendChild(element.note);
     }
 }
--- Note.js
+++ Note.js
@@ -1,6 +1,6 @@
 class Note {
     constructor(value) {
-        let colors = ['rgb(255, 255, 117)', 'rgb(184, 255, 162)', 'rgb(255, 152, 186)', 'rgb(137, 202, 255)']
+        let notesColors = ['rgb(255, 255, 117)', 'rgb(184, 255, 162)', 'rgb(255, 152, 186)', 'rgb(137, 202, 255)']
         this.note = document.createElement('article');
         // this.button = document.createElement('button');
         // this.i = document.createElement('i');
@@ -12,7 +12,7 @@ class Note {
         this.note.innerHTML = `<button><i class="fas fa-trash-alt"></i></button><p>${value}</p>`;
 
         this.note.style.transform = `rotate(${Math.floor((Math.random() * 8) - 4)}deg) scale(1, 1)`;
-        this.note.style.backgroundColor = colors[Math.floor((Math.random() * colors.length))];
+        this.note.style.backgroundColor = notesColors[Math.floor((Math.random() * notesColors.length))];
         // document.querySelector('main').appendChild(this.note);
         // this.note.appendChild(this.button);
         // this.note.appendChild(this.p);
